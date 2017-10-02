a62108536083d9e07f190ddfd95f08bb1a045a0f
Kevin Goldsmith <kevin@unitcircle.com>
2017-10-01 18:11:27 -0700

    getting an error because of utf-8, but it is major surgery, so checkpointing also, got page template going, got page variables going, etc

--- generate-conference-pages.py
+++ generate-conference-pages.py
@@ -1,42 +1,69 @@
 #!/usr/bin/env python
 import json
 from datetime import date
+from string import Template
+import copy
+import unicodedata
+import string
 
+#thanks lazyweb
+#https://stackoverflow.com/questions/295135/turn-a-string-into-a-valid-filename
+validFilenameChars = "-_%s%s" % (string.ascii_letters, string.digits)
+def generateFilename(filename):
+	filename = filename.replace(' ','-')
+	filename = filename.replace('@','at')
+	cleanedFilename = unicodedata.normalize('NFKD', unicode(filename)).encode('ASCII', 'ignore')
+	newFilename = ''.join(c for c in cleanedFilename if c in validFilenameChars)
+	return newFilename.lower() + '.html'
+
+
+#get the data
 with open('conferences.json', 'r') as f:
-    conference_talks = json.load(f)
+	conference_talks = json.load(f)
 
-#for conference in conference_talks:
-#	for talk in conference['talks']:
-#		talkdate = date(*map(int, talk['date'].split("-")))
-#		print("{0} - {1} - {2}".format( conference['conference'].encode('utf-8'),
-#										talk['talk'].encode('utf-8'),
-#										talkdate.strftime("%B %d, %Y")))
+#get the page template
+with open('talkpagetemplate.html') as f:
+	talkpagetemplate = Template( f.read() )
+
+#get the page variables (which becomes our template dictionary)
+#the json file looks like
+#{
+#	"author": "<yourname>",
+#	"googsiteverification": "<your google site verification>",
+#	"googauthor": "<your google plus url>",
+#	"googpublisher": "<your google publisher url>"
+#}
+with open('pagevariables.json') as f:
+	pagevariables = json.load(f)
 
 unique_talks = {}
 
+#conference JSON is organized by conference for ease of editing
+#but for the purposes of creating pages, we want to store by talks
+#so we swizzle
 for conference in conference_talks:
 	for talk in conference['talks']:
 		talk_index = ""
-#		if 'root-talk' in talk:
-#			talk_index = talk['root-talk']
-#		else:
-#			talk_index = talk['talk']
-		
 		talk_index = talk['root-talk'] if 'root-talk' in talk else talk['talk']
-		
 		if len(talk_index) > 0:
 			if talk_index in unique_talks:
 				unique_talks[talk_index].append(conference)
 			else:
-				unique_talks[talk_index]=[]
-				unique_talks[talk_index].append(conference)
+				unique_talks[talk_index]=[conference]
 
+#now walk through our talk list generating pages for each talk
 for talk_index in unique_talks:
-	print talk_index
+	talktitle = talk_index
+	print "generating page for {0}".format(talktitle.encode('utf-8'))
+	filetitle = generateFilename(talktitle)
+	print "creating file: {0}".format(filetitle)
+
+	pagevalues = copy.deepcopy(pagevariables)
 	recordings = []
 	presentations = []
 	reactions = []
 	conferences = unique_talks[talk_index]
+
 	for conference in conferences:
 		this_talk = None
 		for talk in conference['talks']:
@@ -59,19 +86,34 @@ for talk_index in unique_talks:
 					ref = reaction['reference-url'].encode('utf-8')
 					reactions.append("<li><span class=\"quote\">{0}</span> - <a href=\"{1}\">{2}</a></li>".format(quote, ref, credit))
 
-	print "RECORDINGS:"
-	for recording in recordings:
-		print recording
+	if len(recordings) >0:
+		print "RECORDINGS:"
+		for recording in recordings:
+			print recording
+		print
 
-	print "PRESENTED AT:"
-	for presentation in presentations:
-		print presentation
+	if len(presentations) > 0:
+		pagevalues['presentationList'] = '\n'.join(presentations)
+#		print "PRESENTED AT:"
+#		for presentation in presentations:
+#			print presentation
+#		print
+	else:
+		pagevalues['presentationList'] = presentations
+
+	if len(reactions) > 0:
+		reactionstring = '<div id=\"reactions\">\n<div class=\"subheader\">Reactions</div>\n<ul>'
+		reactionstring += '\n'.join(reactions)
+		reactionstring += '</ul></div>'
+		pagevalues['reactions'] = reactionstring
+#		print "REACTIONS:"
+#		for reaction in reactions:
+#			print reaction
+#		print
+	else:
+		pagevalues['reactions'] = ''
+
+	print talkpagetemplate.substitute(pagevalues)
+
 	print
-
-	print "REACTIONS:"
-	for reaction in reactions:
-		print reaction
 	print
-
-	print
-
