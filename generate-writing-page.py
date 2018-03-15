2e646aadb4f8d72908782d240c693a7a629fd475
Kevin <kgoldsmith@avvo.com>
2018-03-14 23:53:44 -0700

    getting tags going

--- generate-writing-page.py
+++ generate-writing-page.py
@@ -5,9 +5,15 @@ import os
 from string import Template
 from datetime import date
 
-format_article_template = Template('<li class=\"article\"><div class=\"articlename\"><a href="$url">$name</a></div><div class=\"articledate\">$formatteddate</div><div class=\"articledescription\">$description</div><ul class="keywordlist">$keywords</ul></li>')
-format_article_keyword = Template('<li class=\"keyword\">$tag</li>')
+format_article_template = Template('<li class=\"article\" data-tags=\"$datatags\"><div class=\"articlename\"><a href="$url">$name</a></div><div class=\"articledate\">$formatteddate</div><div class=\"articledescription\">$description</div><ul class="keywordlist">$keywords</ul></li>')
+format_article_keyword_template = Template('<li class=\"keyword\">$tag</li>')
+format_filter_button_template = Template('<button data-filter=\"ping\" data-filter-tag=\"$tag\">$name</button>')
 output_directory = 'output/'
+
+def tagifyTag(tag):
+	tag = tag.replace(' ', '-')
+	return tag.lower()
+
 
 with open('data/writing.json') as f:
 	writings = json.load(f)
@@ -22,16 +28,23 @@ for writing in writings:
 	if len(writing['name']) > 0:
 		writing['formatteddate'] = date(*map(int, writing['date'].split("-"))).strftime("%B %d, %Y")
 		tag_list = ""
+		data_tag_list = []
 		for tag in writing['tags']:
 			tag_set.add(tag)
-			tag_list += format_article_keyword.substitute({'tag': tag})
+			tag_list += format_article_keyword_template.substitute({'tag': tag})
+			data_tag_list.append(tagifyTag(tag))
 		writing['keywords'] = tag_list
+		writing['datatags'] = ' '.join(data_tag_list)
 		article_list += format_article_template.substitute(writing)
+
+button_list = ""
+for tag in tag_set:
+	button_list += format_filter_button_template.substitute({'name': tag, 'tag': tagifyTag(tag)})
 
 if not os.path.exists(output_directory):
 	os.makedirs(output_directory)
 
-d = dict(writinglist = article_list)
+d = dict(writinglist = article_list, tagbuttons = button_list)
 print("writing writing.html")
 with open(output_directory+'writing.html', 'w') as f:
 	f.write(writingpagetemplate.substitute(d))
