3fcfe2a446487a09c1a01aedc4650d159fe23297
Kevin Goldsmith <kevin@unitcircle.com>
2021-04-04 00:45:05 -0700

    other pages now generated with jinja

--- generate_pages.py
+++ generate_pages.py
@@ -11,16 +11,40 @@ __copyright__ = "Copyright 2021, Kevin Goldsmith"
 __license__ = "MIT"
 __status__ = "Production"                               # Prototype, Development or Production
 
+import os
 import argparse
+import jinja2
 import generate_resume_page_jinja
 import generate_writing_page_jinja
+from common import get_output_directory
+
+
+def generate_other_pages(debug_mode=False):
+    """generate the simpler pages"""
+    other_pages = [
+        ('index-jinja.html', 'site-index-template-jinja.html'),
+        ('music-jinja.html', 'music-template-jinja.html'),
+        ('photography-jinja.html', 'photography-template-jinja.html')
+    ]
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+
+    for page in other_pages:
+        template = env.get_template(page[1])
+        page_data = dict(debug_mode=debug_mode)
+        output_path = os.path.join(get_output_directory(debug_mode), page[0])
+        print('writing: ' + output_path)
+        with open(output_path, 'w') as file:
+            file.write(template.render(page_data))
+
 
 def main(debug_mode=False):
     """call the methods in the other modules"""
-    generate_writing_page_jinja.generate_writing_page(debug_mode=debug_mode,
-        output_file='writing-jinja.html')
-    generate_resume_page_jinja.generate_resume_page(debug_mode=debug_mode,
-        output_file='resume-jinja.html')
+    #generate_writing_page_jinja.generate_writing_page(debug_mode=debug_mode,
+    #    output_file='writing-jinja.html')
+    #generate_resume_page_jinja.generate_resume_page(debug_mode=debug_mode,
+    #    output_file='resume-jinja.html')
+    generate_other_pages(debug_mode)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='generate the writings file')
