a07382ef81595749f870dfdcdfe2160893af0f5e
Kevin Goldsmith <kevin@unitcircle.com>
2021-04-03 18:01:25 -0700

    cleaned up resume generation to use more jinja.

--- generate-resume-page-jinja.py
+++ generate-resume-page-jinja.py
@@ -15,31 +15,26 @@ output_page = 'resume-jinja.html'
 
 
 def format_job_list(work):
-	format_company_name = u'<span itemprop="name">{0}</span>'
-	format_company_url = u'<a class="outbound" href="{0}" target="_blank">{1}</a>'
-	format_job_li = u'<li itemprop="{jobstatus}" itemscope itemtype="http://schema.org/Organization"><div class="job-header"><div class="job-title">{title}</div><div class="job-dates">{from_date} - {to_date}</div><div class="job-company">{company}</div><div class="job-city">{city}</div></div><div class="job-description" itemprop="description">{description}</div></li>'
-
 	job_list = []
 	for work_item in work:
-		company_name = format_company_name.format(work_item['company'])
 		if 'website' in work_item:
 			validate_url(work_item['website'])
 		d = dict(
 			title=work_item['position'],
 			from_date=format_month_year_from_string(work_item['startDate']),
 			to_date=format_month_year_from_string(work_item['endDate']) if 'endDate' in work_item else 'present',
-			company=format_company_url.format(work_item['website'], company_name) if 'website' in work_item else company_name,
+			company=work_item['company'],
+			website=work_item.get('website'),
 			city=work_item['location'],
 			description=generate_paragraphs_for_lines(work_item['summary']),
 			jobstatus='alumniOf' if 'endDate' in work_item else 'worksFor'
 			)
-		job_list.append(format_job_li.format(**d))
+		job_list.append(d)
 
-	return u'\n'.join(job_list)
+	return job_list
 
 
 def format_patent_list(patents):
-	format_patent_li = '<li><div class="patent-header"><div class="patent-title"><a class="outbound" href="{url}" target="_blank">{title}</a></div><div class="patent-number">#{number}</div><div class="patent-authorship">{author}</div><div class="patent-filing-date">filed: {filing_date}</div><div class="patent-granted-date">granted: {granted_date}</div></div><div class="patent-abstract">{abstract}</div></li>'
 	patent_list = []
 	for patent in patents:
 		validate_url(patent['url'])
@@ -52,41 +47,28 @@ def format_patent_list(patents):
 			granted_date=format_month_day_year_from_string(patent['grantedDate']),
 			abstract=patent['abstract']
 			)
-		patent_list.append(format_patent_li.format(**d))
+		patent_list.append(d)
 
-	return u'\n'.join(patent_list)
+	return patent_list
 
 
 #no point in treating this like a list, I only have the one degree :)
 def format_education(education_item):
-	format_education_text='<span itemprop="alumniOf" itemscope itemtype="http://schema.org/CollegeOrUniversity"><a class="outbound" itemprop="sameAs" href="{website}"><span class="university-name" itemprop="name">{institution}</span></a>, {location}. {studyType} Degree in {area}, graduated {grad_date}. {summary}</span>'
-	education_item['grad_date']=format_month_year_from_string(education_item['endDate'])
-	return format_education_text.format(**education_item)
+	d = education_item.copy()
+	d['grad_date'] = format_month_year_from_string(education_item['endDate'])
+	return d
 
 
 def format_interview_list(interviews):
-	format_interview_li = u'<li><a class="outbound" href="{url}">{name}</a>, {date}, {credit}</li>'
-	format_credit_author_publisher = '{0} - {1}'
-	format_credit_either = '{0}'
-
 	interview_list = []
 	for interview in interviews:
 		validate_url(interview['url'])
-		if ('author' in interview) and ('publisher' in interview):
-			interview['credit'] = format_credit_author_publisher.format(interview['author'], interview['publisher'])
-		else:
-			interview['credit'] = format_credit_either.format(interview['author'] if 'author' in interview else interview['publisher'])
-
 		interview['date'] = format_month_year_from_string(interview['date'])
-		interview_list.append(format_interview_li.format(**interview))
-	return u'\n'.join(interview_list)
+		interview_list.append(interview)
+	return interview_list
 
 
 def format_keynote_list(conferences):
-	format_keynote_li = '<li><span class="talk-title">{talk}</span>, {conference}, {date}, {location}</li>'
-	format_conference_with_link='<a class="outbound" href="{conference_url}">{conference_name}</a>'
-	format_talk_with_link='<a href="{talk_url}">{talk}</a>'
-
 	keynote_list = []
 	for conference in conferences:
 		for talk in conference['talks']:
@@ -98,13 +80,15 @@ def format_keynote_list(conferences):
 				else:
 					validate_url(talk['talk-url'])
 				d = dict(
-					talk=format_talk_with_link.format(**dict(talk_url=talk['talk-url'], talk=talk['talk'])) if 'talk-url' in talk else talk['talk'],
-					conference=format_conference_with_link.format(**dict(conference_url=conference['url'],conference_name=conference['conference'])) if 'url' in conference else conference['conference'],
+					talk=talk['talk'],
+					talk_url=talk.get('talk-url'),
+					conference=conference['conference'],
+					conference_url=conference.get('url'),
 					date=format_month_year_from_string(talk['date']),
 					location=format_city_state_country_from_location(talk['location']) if 'location' in talk else 'virtual'
 					)
-				keynote_list.append(format_keynote_li.format(**d))
-	return '\n'.join(keynote_list)
+				keynote_list.append(d)
+	return keynote_list
 
 
 # get the template
