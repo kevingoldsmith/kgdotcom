6087013a4f0f03dd41c2924aa25b308e67fd48f8
Kevin Goldsmith <kevin@unitcircle.com>
2022-12-04 17:15:48 -0800

    improved directory walking, creating Image objects with EXIF and local json file data overrides

--- generate_photo_pages.py
+++ generate_photo_pages.py
@@ -9,25 +9,29 @@ __version__ = "3.0.0"
 __author__ = "Kevin Goldsmith"
 __copyright__ = "Copyright 2022, Kevin Goldsmith"
 __license__ = "MIT"
-__status__ = "Production"  # Prototype, Development or Production
+__status__ = "Development"  # Prototype, Development or Production
 
-import fnmatch
 import json
 import logging
 import os
 
+from PIL import Image as PILImage
+from PIL.ExifTags import TAGS
+
 
 __PHOTOS_DIRECTORY = "photos"
+__GALLERY_PHOTO_MAX = (2000,2000)
+__GALLERY_THUMB_MAX = (1000,1000)
 
 class Gallery:
-    def __init__(self, name, directory) -> None:
+    def __init__(self, name:str, directory:str) -> None:
         self.name = name
         self.directory = directory
         self.sub_galleries = []
         self.images = []
 
     def __str__(self) -> str:
-        return f"{self.name} / subgalleries: {self.sub_galleries} / images: {len(self.images)}"
+        return f"{self.name} / subgalleries: {self.sub_galleries} / images: {self.images}"
     
     def __repr__(self) -> str:
         return str(self)
@@ -36,8 +40,8 @@ class Gallery:
         items = os.listdir(self.directory)
         for item in items:
             path = os.path.join(self.directory, item)
-            if is_image_file(path):
-                self.images.append(item)
+            if Image.is_image_file(path):
+                self.images.append(Image(item, path))
             elif os.path.isdir(path):
                 newgal = Gallery(item, path)
                 newgal.populate()
@@ -45,9 +49,45 @@ class Gallery:
                     self.sub_galleries.append(newgal)
         pass
 
+class Image:
+    def __init__(self, name:str, path:str) -> None:
+        self.name = name
+        self.path = path
+        self.exif = {}
+        self.initialize_EXIF()
+        self.load_JSON_overrides()
 
-def is_image_file(filename, extensions=['.jpg', '.jpeg', '.gif', '.png']):
-    return any(filename.endswith(e) for e in extensions)
+    def __str__(self) -> str:
+        return f"{self.name} - {self.path}"
+
+    def __repr__(self) -> str:
+        return str(self)
+    
+    def initialize_EXIF(self) -> None:
+        pil_image = PILImage.open(self.path)
+        self.exif['format'] = pil_image.format
+        self.exif['size'] = pil_image.size
+        exif_tags = pil_image.getexif()
+        for tag_id in exif_tags:
+            # get the tag name, instead of human unreadable tag id
+            tag = TAGS.get(tag_id, tag_id)
+            data = exif_tags.get(tag_id)
+            # decode bytes 
+            if isinstance(data, bytes):
+                data = data.decode()
+            self.exif[tag] = data
+    
+    def load_JSON_overrides(self) -> None:
+        root_name = os.path.splitext(self.path)[0]
+        json_file = root_name + ".json"
+        if os.path.exists(json_file):
+            with open(json_file, 'r') as f:
+                image_data = json.load(f)
+                self.name = image_data.get('name', self.name)
+                self.description = image_data.get('description', '')
+
+    def is_image_file(filename:str, extensions=['.jpg', '.jpeg', '.gif', '.png']):
+        return any(filename.endswith(e) for e in extensions)
 
 
 def generate_photo_pages(debug_mode: bool = False) -> None:
