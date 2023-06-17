bc20d0445f8fafa3454038e8e909f9bfa56dfd9d
Kevin Goldsmith <kevin@unitcircle.com>
2023-06-16 22:02:05 -0700

    updated web page and paths

--- generate_contact_pages.py
+++ generate_contact_pages.py
@@ -26,7 +26,7 @@ from datetime import datetime
 
 logger = logging.getLogger()
 
-def generate_contact_page(data:dict, debug_mode:bool = False) -> None:
+def generate_contact_page(data:dict, output_directory:str, debug_mode:bool = False) -> None:
     """
     generate_contact_page _summary_
 
@@ -41,18 +41,19 @@ def generate_contact_page(data:dict, debug_mode:bool = False) -> None:
     env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
     pagetemplate = env.get_template("contactpage.html")
 
-    output_path = os.path.join(common.get_output_directory(debug_mode), data['filename'])
+    output_path = os.path.join(output_directory, data['filename'])
     logger.info(f"writing: {output_path}")
     with open(output_path, "w") as file:
         file.write(pagetemplate.render(data))
 
 
-def generate_card_file(data:dict, debug_mode:bool = False) -> None:
+def generate_card_file(data:dict, output_directory:str, debug_mode:bool = False) -> None:
     """
     generate_card_file _summary_
 
     Args:
         data (dict): _description_
+        output_directory(str): the output path
         debug_mode (bool, optional): _description_. Defaults to False.
     """
     logger.debug("generating %s", data['vcf_filename'])
@@ -68,7 +69,7 @@ def generate_card_file(data:dict, debug_mode:bool = False) -> None:
     env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
     pagetemplate = env.get_template("contactfile.vcf")
 
-    output_path = os.path.join(common.get_output_directory(debug_mode), data['vcf_filename'])
+    output_path = os.path.join(output_directory, data['vcf_filename'])
     logger.info(f"writing: {output_path}")
     with open(output_path, "w") as file:
         file.write(pagetemplate.render(data))
@@ -89,13 +90,16 @@ def generate_contact_pages(debug_mode:bool = False) -> None:
         common_data = json.load(file)
 
     common_contact_data = contact_me_data['common']
+    output_directory = os.path.join(common.get_output_directory(debug_mode), "contact")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
     for page in contact_me_data['contacts']:
         merged_data = common_data.copy()
         merged_data.update(common_contact_data)
         merged_data.update(page)
-        generate_contact_page(merged_data, debug_mode)
-        generate_card_file(merged_data, debug_mode)
+        generate_contact_page(merged_data, output_directory, debug_mode)
+        generate_card_file(merged_data, output_directory, debug_mode)
 
 
 if __name__ == "__main__":
