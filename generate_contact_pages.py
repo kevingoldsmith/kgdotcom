aa0fb46cc77a914c296f61733ecdbf17165e4499
Kevin Goldsmith <kevin@unitcircle.com>
2023-06-21 15:44:13 +0200

    cleaned up styling, finished first version of personal and work pages

--- generate_contact_pages.py
+++ generate_contact_pages.py
@@ -26,28 +26,8 @@ from datetime import datetime
 
 logger = logging.getLogger()
 
-def generate_contact_page(data:dict, output_directory:str, debug_mode:bool = False) -> None:
-    """
-    generate_contact_page _summary_
 
-    Args:
-        data (dict): _description_
-        debug_mode (bool, optional): _description_. Defaults to False.
-    """
-    print(data)
-    logger.debug("generating %s", data['filename'])
-
-    # get the template
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
-    pagetemplate = env.get_template("contactpage.html")
-
-    output_path = os.path.join(output_directory, data['filename'])
-    logger.info(f"writing: {output_path}")
-    with open(output_path, "w") as file:
-        file.write(pagetemplate.render(data))
-
-
-def generate_card_file(data:dict, output_directory:str, debug_mode:bool = False) -> None:
+def generate_card_file(data:dict, output_directory:str, debug_mode:bool = False) -> str:
     """
     generate_card_file _summary_
 
@@ -55,12 +35,15 @@ def generate_card_file(data:dict, output_directory:str, debug_mode:bool = False)
         data (dict): _description_
         output_directory(str): the output path
         debug_mode (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        the name of the generated card file
     """
     logger.debug("generating %s", data['vcf_filename'])
 
     data['rev'] = datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
-    if 'image' in data:
-        with open(os.path.join('assets', data['image']), 'rb') as image_file:
+    if 'card_photo' in data:
+        with open(os.path.join('assets', data['card_photo']), 'rb') as image_file:
             image_data = image_file.read()
             base64_image = base64.b64encode(image_data)
             data['photo_b64'] = base64_image.decode('utf-8')
@@ -70,6 +53,34 @@ def generate_card_file(data:dict, output_directory:str, debug_mode:bool = False)
     pagetemplate = env.get_template("contactfile.vcf")
 
     output_path = os.path.join(output_directory, data['vcf_filename'])
+    logger.info(f"writing: {output_path}")
+    with open(output_path, "w") as file:
+        file.write(pagetemplate.render(data))
+
+    return data['vcf_filename']
+
+
+def generate_contact_page(data:dict, output_directory:str, card_file_path:str, debug_mode:bool = False) -> None:
+    """
+    generate_contact_page _summary_
+
+    Args:
+        data (dict): _description_
+        output_directory(str): the output path
+        card_file_path (str): the path of the card file to link to
+        debug_mode (bool, optional): _description_. Defaults to False.
+    """
+    data['card_file_path'] = card_file_path
+    data['card_photo'] = os.path.join('..', 'img', data['card_photo'])
+    if 'header_image' in data:
+        data['header_image'] = os.path.join('..', 'img', data['header_image'])    
+    logger.debug("generating %s", data['filename'])
+
+    # get the template
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
+    pagetemplate = env.get_template("contactpage.html")
+
+    output_path = os.path.join(output_directory, data['filename'])
     logger.info(f"writing: {output_path}")
     with open(output_path, "w") as file:
         file.write(pagetemplate.render(data))
@@ -98,8 +109,8 @@ def generate_contact_pages(debug_mode:bool = False) -> None:
         merged_data = common_data.copy()
         merged_data.update(common_contact_data)
         merged_data.update(page)
-        generate_contact_page(merged_data, output_directory, debug_mode)
-        generate_card_file(merged_data, output_directory, debug_mode)
+        card_file = generate_card_file(merged_data, output_directory, debug_mode)
+        generate_contact_page(merged_data, output_directory, card_file, debug_mode)
 
 
 if __name__ == "__main__":
